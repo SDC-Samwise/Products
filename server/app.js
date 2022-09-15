93ac5bab28ea7584dd7348a1efc52c9369b9557a
Johnny <Johnnyho9293@gmail.com>
2022-09-14 18:58:00 -0700

    created schema and table for database

--- server/app.js
+++ server/app.js
@@ -25,8 +25,11 @@ app.get('/products/:product_id', (req, res) => {
   let getQuery = `Select * from public.products
   where id=${req.params.product_id}`
 
-  let getQuery2 = `Select * from public.products
-  where id=${req.params.product_id}`
+  let getQuery2 = `Select public.products, json_agg(json_build_object('feature', feature, 'value', value)) AS agg
+  FROM public.features_feature JOIN (
+    SELECT code from project
+  ) AS p ON p.code=activity.pcode
+  group by id;`
 
   // let getQuery2 = `SELECT public.reviews, json_agg(json_build_object('id', id, 'url', url)) AS agg
   // FROM public.reviews_photos JOIN (
