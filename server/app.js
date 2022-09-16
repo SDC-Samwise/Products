3a6c27f83874765837496880f486afbfb8f5d8df
Johnny <Johnnyho9293@gmail.com>
2022-09-16 10:48:11 -0700

    Query updates

--- server/app.js
+++ server/app.js
@@ -47,7 +47,7 @@ console.log('product related', req.params.product_id)
 
 let getQuery = `select json_agg(related_product_id) AS related from related
 WHERE current_product_id = ${req.params.product_id}
-GROUP BY current_product_id;`
+GROUP BY current_product_id;`;
 
   client.query(getQuery, (err, result)=>{
     if(!err){
@@ -66,10 +66,32 @@ app.get('/products/:product_id/styles', (req, res) => {
 
   console.log('product styles', req.params.product_id)
 
-  let getQuery = `select pd.*, json_agg(json_build_object('feature', ft.id, 'value', ft.value)) AS features
-  from products pd
-  left join features ft on ft.product_id=pd.id where pd.id = ${req.params.product_id}
-  group by pd.id`
+  let getQuery = `select json_build_object
+  (
+  'product_id', st.productId,
+  'results', (select json_agg(json_build_object(
+                'style_id', st.id,
+                  'name', st.name,
+                  'original_price', st.original_price,
+                  'sale_price', st.sale_price,
+                  'default?', st.default_style,
+                  'photos', (select json_agg(json_build_object('tumbnail_url', ph.thumbnail_url, 'url', ph.url))
+                       FROM photos ph
+                       WHERE ph.styleid = st.id
+                       GROUP BY ph.styleid
+                       ),
+                 'skus', (SELECT json_object_agg(
+                      st.id, (SELECT json_build_object('quantity', st.quantity, 'size', st.size)))
+                         FROM skus st
+                         WHERE st.styleid = 1
+                         GROUP BY st.styleid)
+              ))
+             WHERE st.productid = 1
+             GROUP BY st.productid)
+  )
+  from styles st
+  WHERE st.productId = 1
+  GROUP BY st.productId;`;
 
   client.query(getQuery, (err, result)=>{
     if(!err){
@@ -81,41 +103,6 @@ app.get('/products/:product_id/styles', (req, res) => {
   });
 
   client.end;
-
-  `select json_build_object ('product_id', st.productId,
-  'results', ( select json_agg(json_build_object('style_id', st.id, 'name', st.name, 'original_price', st.original_price, 'sale_price', st.sale_price, 'default?', st.default_style))
-        WHERE st.productid = 1
-        GROUP BY st.productid),
-  'photos', (select json_agg(json_build_object('tumbnail_url', ph.thumbnail_url, 'url', ph.url))
-         FROM photos ph
-         WHERE ph.styleid = 1
-         GROUP BY ph.styleid
-        )
-  )
-from styles st
-WHERE st.productId = 1
-GROUP BY st.productId;`
-
-//select results within results
-`select json_build_object ('product_id', st.productId,
-'results', ( select json_agg(json_build_object('style_id', st.id, 'name', st.name, 'original_price', st.original_price, 'sale_price', st.sale_price, 'default?', st.default_style,
-                        'photos', (select json_agg(json_build_object('tumbnail_url', ph.thumbnail_url, 'url', ph.url))
-                             FROM photos ph
-                             WHERE ph.styleid = st.id
-                             GROUP BY ph.styleid
-                            ),
-                       'skus', (select json_agg(json_build_object('quantity', sk.quantity, 'size', sk.size))
-                             FROM skus sk
-                             WHERE sk.styleid = st.id
-                             GROUP BY sk.styleid
-                            )
-                      ))
-      WHERE st.productid = 1
-      GROUP BY st.productid)
-)
-from styles st
-WHERE st.productId = 1
-GROUP BY st.productId;`
 
 });
 
