d1308b0d32d69cc08ab3851576f3159d452a0325
Johnny <Johnnyho9293@gmail.com>
2022-09-16 10:47:34 -0700

    Added styles query

--- server/app.js
+++ server/app.js
@@ -41,9 +41,9 @@ app.get('/products/:product_id', (req, res) => {
 
 });
 
-app.get('/products/:product_id/styles', (req, res) => {
+app.get('/products/:product_id/related', (req, res) => {
 
-console.log('product styles', req.params.product_id)
+console.log('product related', req.params.product_id)
 
 let getQuery = `select json_agg(related_product_id) AS related from related
 WHERE current_product_id = ${req.params.product_id}
@@ -62,19 +62,69 @@ GROUP BY current_product_id;`
 
 });
 
-app.get('/products/:product_id/related', (req, res) => {
+app.get('/products/:product_id/styles', (req, res) => {
 
-  // let getQuery = `Select * from public.products
-  // ORDER BY id ASC LIMIT 100`
+  console.log('product styles', req.params.product_id)
 
-  // client.query(getQuery, (err, result)=>{
-  //   if(!err){
-  //       res.send(result.rows);
-  //   }
-  // });
+  let getQuery = `select pd.*, json_agg(json_build_object('feature', ft.id, 'value', ft.value)) AS features
+  from products pd
+  left join features ft on ft.product_id=pd.id where pd.id = ${req.params.product_id}
+  group by pd.id`
 
-  // client.end;
+  client.query(getQuery, (err, result)=>{
+    if(!err){
+      res.send(result.rows);
+    } else {
+      console.log(err.message);
+      res.send(err.message)
+    }
+  });
+
+  client.end;
+
+  `select json_build_object ('product_id', st.productId,
+  'results', ( select json_agg(json_build_object('style_id', st.id, 'name', st.name, 'original_price', st.original_price, 'sale_price', st.sale_price, 'default?', st.default_style))
+        WHERE st.productid = 1
+        GROUP BY st.productid),
+  'photos', (select json_agg(json_build_object('tumbnail_url', ph.thumbnail_url, 'url', ph.url))
+         FROM photos ph
+         WHERE ph.styleid = 1
+         GROUP BY ph.styleid
+        )
+  )
+from styles st
+WHERE st.productId = 1
+GROUP BY st.productId;`
+
+//select results within results
+`select json_build_object ('product_id', st.productId,
+'results', ( select json_agg(json_build_object('style_id', st.id, 'name', st.name, 'original_price', st.original_price, 'sale_price', st.sale_price, 'default?', st.default_style,
+                        'photos', (select json_agg(json_build_object('tumbnail_url', ph.thumbnail_url, 'url', ph.url))
+                             FROM photos ph
+                             WHERE ph.styleid = st.id
+                             GROUP BY ph.styleid
+                            ),
+                       'skus', (select json_agg(json_build_object('quantity', sk.quantity, 'size', sk.size))
+                             FROM skus sk
+                             WHERE sk.styleid = st.id
+                             GROUP BY sk.styleid
+                            )
+                      ))
+      WHERE st.productid = 1
+      GROUP BY st.productid)
+)
+from styles st
+WHERE st.productId = 1
+GROUP BY st.productId;`
+
 });
+
+app.post('/cart', (req, res) => {
+  console.log('product styles', req.body)
+
+    res.send(req.body);
+
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
